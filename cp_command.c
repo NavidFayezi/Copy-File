a1aec94642a1042428a91d4606c53ce2dc71051d
Navid <navidfayezi.98@gmail.com>
2025-05-09 13:24:45 -0600

    Add copy_file() fucntion, works as expected

--- cp_command.c
+++ cp_command.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
-#include<stdlib.h> // Needed for exit()
-#include<fcntl.h> // This includes file manipulation system calls
-#include<errno.h>
+#include<stdlib.h>  // Needed for exit().
+#include<fcntl.h>   // This includes file manipulation system calls.
+#include<errno.h>   // Includes errno which is used in error handling.
 #include<string.h>
+#include<sys/types.h>   // Include standard data types. 
+#include<unistd.h>  // read() is declared here.
+#include<sys/stat.h>    // Defines mode_t permission constants
+
+
+// Declare function prototype
+void copy_file(int, int);
 
 
 int main(int argc, char *argv[]){
@@ -12,29 +19,82 @@ int main(int argc, char *argv[]){
     correct.
     */
     if (argc != 3){
-        printf("Bad arguments. Use cp_command source_file destination_file\n");
+        printf("Bad arguments. Use cp_command source_file destination_file.\n");
+        exit(EXIT_FAILURE);
+    }
+        
+
+    // Declare variables for the open() call to read
+    char *source_file = argv[1];
+    int file_descriptor_read;
+    int open_r_flags = O_RDONLY;
+    
+    // variables required for the open() call to write
+    char *destination_file = argv[2];
+    int open_w_flags = O_WRONLY | O_TRUNC | O_APPEND | O_CREAT;
+    mode_t w_permissions = S_IRWXU | S_IRWXG;
+    int file_descriptor_write;
+
+
+    // Open the file specified by the input to read from.
+    file_descriptor_read = open(source_file, open_r_flags);
+
+    // open() failed. 
+    if(file_descriptor_read == -1){
+        printf("DEBUG: Open to read Failed.\n");
+        perror(strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    else{
-        
-        // Declare variables
-        char *source_file = argv[1];
-        char *destination_file = argv[2];
-        int file_descriptor_open;
-        int open_flags = O_RDONLY;
+    // Open the file specified by the input to write to. 
+    file_descriptor_write = open(destination_file, open_w_flags, 
+                                    w_permissions);
 
-        file_descriptor_open = open(source_file, open_flags);
+    // Second open() failed.
+    if (file_descriptor_write == -1){
+        printf("DEBUG: Open to write Failed.\n");
+        perror(strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    
+    // Copy content of source to destination
+    copy_file(file_descriptor_read, file_descriptor_write);
+    
+
+    return 0;
+}
+
+
+void copy_file(int fd_source, int fd_destination){
+    
+    // variables required for the read() call
+    size_t count = 1024;
+    void *data_read = malloc(count + 1);
+    ssize_t bytes_read = 0;    
+    ssize_t bytes_wrote = 0;
+    
+    do {
+        bytes_read = read(fd_source, data_read, count);
         
-        // open() failed. 
-        if(file_descriptor_open == -1){
+        // Error checking for read()
+        if (bytes_read == -1){
+            printf("DEBUG: read() failed in copy_file().\n");
             perror(strerror(errno));
             exit(EXIT_FAILURE);
         }
-        else{
-            printf("If not taken, opened without errors.");
 
+        bytes_wrote = write(fd_destination, data_read, bytes_read);
+
+        // Error checking for write()
+        if (bytes_wrote == -1){
+            printf("DEBUG: write() failed in copy_file().\n");
+            perror(strerror(errno));
+            exit(EXIT_FAILURE);
         }
-        
-    }
+
+    } while (bytes_read > 0);
+
+    
+    free(data_read);
 }
